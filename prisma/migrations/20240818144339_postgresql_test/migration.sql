71942e548af95957f663465c1481d05a0bcaa369
Lluís Alcover Serra <lluisalcover1@gmail.com>
2024-08-18 17:42:31 +0200

    fix: files fixes

--- prisma/migrations/20240818144339_postgresql_test/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Book" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "genre" TEXT NOT NULL,
-
-    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
-);
